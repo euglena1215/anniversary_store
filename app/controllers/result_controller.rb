3d69a26065421fdf14604c05dfe4114f7fe3761c
Shintani Teppei <teppest1215@gmail.com>
2018-09-07 12:44:18 +0900

    ログインを抜いた

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -1,13 +1,14 @@
 class ResultController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def index
-    @me = current_user
-    @self_evaluations = Evaluation.where(user: current_user)
+    # @me = current_user
+    @me = User.find(1) # ケンイチに固定
+    @self_evaluations = Evaluation.where(user: @me)
 
     # coupleが同時に複数いない前提
-    couple = Couple.where(user1: current_user).or(Couple.where(user2: current_user)).where(ended_at: nil).first
-    @partner = couple.user1 == current_user ? couple.user2 : couple.user1
+    couple = Couple.where(user1: @me).or(Couple.where(user2: @me)).where(ended_at: nil).first
+    @partner = couple.user1 == @me ? couple.user2 : couple.user1
     @partner_evaluations = Evaluation.where(user: @partner)
 
     @event = Event.find_by(id: params[:event_id])
